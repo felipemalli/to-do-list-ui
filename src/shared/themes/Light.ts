2b5c555e217a1adcb251216081550080174e5cfc
felipemalli <felipevahia@gmail.com>
2022-08-06 02:53:40 -0300

    feat: implement light theme and provider

---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,24 @@
+import { createTheme } from '@mui/material';
+import { cyan, orange } from '@mui/material/colors';
+
+export const LightTheme = createTheme({
+  palette: {
+    primary: {
+      main: orange[700],
+      dark: orange[800],
+      light: orange[500],
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: cyan[500],
+      dark: cyan[600],
+      light: cyan[300],
+      contrastText: '#ffffff',
+    },
+    background: {
+      default: '#f7f6f3',
+      paper: '#ffffff',
+    }
+  }
+});
+
