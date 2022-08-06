606cb3f59e0fce223a464063b2b17376cbfe28b2
felipemalli <felipevahia@gmail.com>
2022-08-06 01:41:45 -0300

    feat: implement react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,13 @@
-export { };
+import { Navigate, Route, Routes } from "react-router-dom";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/inicialPage" element={<p>Página inicial teste</p>} />
+
+      <Route path="*" element={<Navigate to="/inicialPage" />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
