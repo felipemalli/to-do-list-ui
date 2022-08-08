a2272b9af832a6372ad34a5916a25ba8d3134d8c
felipemalli <felipevahia@gmail.com>
2022-08-08 11:33:47 -0300

    feat: implement toggle theme

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { useAppThemeContext } from "../shared/contexts";
 
 export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
+
   return (
     <Routes>
-      <Route path="/inicialPage" element={<Button variant='contained'>Teste</Button>} />
+      <Route path="/inicialPage" element={<Button variant='contained' onClick={toggleTheme}>Teste</Button>} />
 
       <Route path="*" element={<Navigate to="/inicialPage" />} />
     </Routes>
