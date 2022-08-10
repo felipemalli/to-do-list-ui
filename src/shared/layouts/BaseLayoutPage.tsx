cf3ccfad33832081f162051e52ebf6a985fa4d07
felipemalli <felipevahia@gmail.com>
2022-08-10 12:31:20 -0300

    feat: implement responsivity for header and add toolbar

--- src/shared/layouts/BaseLayoutPage.tsx
+++ src/shared/layouts/BaseLayoutPage.tsx
@@ -1,36 +1,46 @@
-import { Divider, Icon, IconButton, Typography, useTheme } from '@mui/material';
+import { Icon, IconButton, Theme, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { grey } from '@mui/material/colors';
 import { Box } from '@mui/system';
+import { ReactNode } from 'react';
 import { useDrawerContext } from '../contexts';
 import { IChildrenProps } from '../interfaces';
 
 interface IBaseLayoutPage extends IChildrenProps {
   title: string;
+  toolbar: ReactNode;
+  
 }
 
-export const BaseLayoutPage: React.FC<IBaseLayoutPage> = ({ children, title }: IBaseLayoutPage) => {
+export const BaseLayoutPage: React.FC<IBaseLayoutPage> = ({ children, title, toolbar }: IBaseLayoutPage) => {
   const theme = useTheme();
+  const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
+  const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
+
   const { toggleDrawerOpen } = useDrawerContext();
 
   return (
-    <Box height="100%" display="flex" flexDirection="column">
-      <Box padding={1} display="flex" alignItems="center" height={theme.spacing(12)} gap={1} bgcolor={grey[200]}>
+    <Box height="100%" display="flex" flexDirection="column" gap={1}>
+      <Box padding={1} display="flex" alignItems="center" gap={1} bgcolor={grey[200]} height={theme.spacing(smDown ? 6 : mdDown ? 8 : 10)}>
         <IconButton onClick={toggleDrawerOpen}>
           <Icon>menu</Icon>
         </IconButton>
-        <Typography variant="h5">
+        <Typography 
+          overflow="hidden"
+          whiteSpace="nowrap"
+          textOverflow="ellipsis"
+          variant={smDown ? 'h5' : 'h4'}
+        >
           {title}
         </Typography>
       </Box>
 
-
-      <Divider  />
+      {toolbar && (
+        <Box>
+          {toolbar}
+        </Box>
+      )}
       
-      <Box>
-        Barra
-      </Box>
-      
-      <Box>
+      <Box flex={1} overflow="auto">
         {children}
       </Box>
     </Box>
