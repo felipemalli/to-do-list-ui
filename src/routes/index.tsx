d9d14ff0f4ffb0ea80eb07cdf354005a371304c0
felipemalli <felipevahia@gmail.com>
2022-08-10 02:56:52 -0300

    feat: implement dynamic item list addition to drawer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,23 @@
 import { Button } from '@mui/material';
+import { useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { useAppThemeContext } from '../shared/contexts';
+import { useDrawerContext } from '../shared/contexts';
 
 export function AppRoutes() {
-  const { toggleTheme } = useAppThemeContext();
+  // const { toggleTheme } = useAppThemeContext();
+  const { toggleDrawerOpen, setDrawerOptions } = useDrawerContext();
+
+  useEffect(() => {
+    setDrawerOptions([
+      { icon: 'home', path: '/initialPage', label:  'Tasks' },
+      { icon: 'person', path: '/profile', label:  'Profile' },
+    ]);
+  }, []);
 
   return (
     <Routes>
-      <Route path="/initialPage" element={<Button variant="contained" onClick={toggleTheme}>Teste</Button>} />
+      {/* <Route path="/initialPage" element={<Button variant="contained" onClick={toggleTheme}>Test</Button>} /> */}
+      <Route path="/initialPage" element={<Button variant="contained" onClick={toggleDrawerOpen}>Test</Button>} />
 
       <Route path="*" element={<Navigate to="/initialPage" />} />
     </Routes>
--- src/shared/components/aside-drawer/AsideDrawer.tsx
+++ src/shared/components/aside-drawer/AsideDrawer.tsx
@@ -1,13 +1,45 @@
-import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from '@mui/material';
+import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
 import { Box } from '@mui/system';
+import { useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
+import { useDrawerContext } from '../../contexts';
 import { IChildrenProps } from '../../interfaces';
+
+interface IListItemLinkProps {
+  to: string;
+  icon: string;
+  label: string;
+  onClick: (() => void) | undefined;
+}
+
+const ListItemLink: React.FC<IListItemLinkProps> = ({ to, icon, label, onClick }: IListItemLinkProps) => {
+  const navigate = useNavigate();
+
+  const resolvedPath = useResolvedPath(to);
+  const match = useMatch({ path: resolvedPath.pathname, end: false });
+
+  const handleClick = () => {
+    navigate(to);
+    onClick?.();
+  };
+
+  return (
+    <ListItemButton selected={!!match} onClick={handleClick}>
+      <ListItemIcon>
+        <Icon>{icon}</Icon>
+      </ListItemIcon>
+      <ListItemText primary={label} />
+    </ListItemButton>
+  );
+};
 
 export const AsideDrawer: React.FC<IChildrenProps> = ({ children }: IChildrenProps ) => {
   const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+  const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext();
 
   return (
     <>
-      <Drawer variant='permanent'>
+      <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'persistent'} onClose={toggleDrawerOpen}>
         <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection={'column'}>
 
           <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
@@ -20,25 +52,22 @@ export const AsideDrawer: React.FC<IChildrenProps> = ({ children }: IChildrenPro
 
           <Box flex={1}>
             <List component="nav">
-              <ListItemButton>
-                <ListItemIcon>
-                  <Icon>home</Icon>
-                </ListItemIcon>
-                <ListItemText primary="Tasks" />
-              </ListItemButton>
-              <ListItemButton>
-                <ListItemIcon>
-                  <Icon>person</Icon>
-                </ListItemIcon>
-                <ListItemText primary="Profile" />
-              </ListItemButton>
+              {drawerOptions.map(({ icon, path, label }) => (
+                <ListItemLink
+                  key={path}
+                  icon={icon}
+                  to={path}
+                  label={label}
+                  onClick={smDown ? toggleDrawerOpen : undefined}
+                />
+              ))}
             </List>
           </Box>
 
         </Box>
       </Drawer>
 
-      <Box height="100vh" marginLeft={theme.spacing(28)}>
+      <Box height="100vh" marginLeft={smDown ? 0 : ( isDrawerOpen ? theme.spacing(28) : 0)}>
         {children}
       </Box>
     </>
