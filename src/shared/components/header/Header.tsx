33688210e2e087674f0bb072d928bc8c391b241d
felipemalli <felipevahia@gmail.com>
2022-08-17 00:46:41 -0300

    style: implement more flexibility for header

--- src/shared/components/header/Header.tsx
+++ src/shared/components/header/Header.tsx
@@ -5,27 +5,28 @@ export const Header: React.FC<{title: string}> = ({ title }: {title: string} ) =
   const theme = useTheme();
   const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
   const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
+  const lgDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('lg'));
   
   const { toggleDrawerOpen, isDrawerOpen } = useDrawerContext();
   const { toggleTheme, themeName } = useAppThemeContext();
 
   return (
     <Box borderLeft={1} borderColor={theme.palette.secondary.main} padding={1} display="flex" justifyContent={'space-between'} alignItems="center" bgcolor={theme.palette.secondary.light} height={theme.spacing(smDown ? 6 : mdDown ? 8 : 10)}>
-      <Box display="flex" gap={smDown ? 2 : 5} alignItems="center">
+      <Box display="flex" gap={mdDown ? 2 : 5} alignItems="center">
         <IconButton onClick={toggleDrawerOpen}>
-          <Icon>menu</Icon>
+          <Icon>{isDrawerOpen ? 'menu_open' : 'menu'}</Icon>
         </IconButton>
         
         <Typography 
           overflow="hidden"
           whiteSpace="nowrap"
           textOverflow="ellipsis"
-          variant={smDown ? 'h5' : 'h4'}
+          variant={mdDown ? 'h5' : 'h4'}
         >
           {title}
         </Typography>
 
-        {!mdDown && 
+        {!((smDown) || (mdDown && isDrawerOpen)) && 
             <Box display="flex" gap={1}>
               <TextField size="small" placeholder='Searching...'/>
               <Box flex={1} display="flex" justifyContent={'end'}>
@@ -40,7 +41,7 @@ export const Header: React.FC<{title: string}> = ({ title }: {title: string} ) =
       </Box>
       
       {
-        !(smDown || (mdDown && isDrawerOpen)) && 
+        !(mdDown || (lgDown && isDrawerOpen)) && 
             <Box padding={2}>
               <IconButton onClick={toggleTheme} >
                 <Icon>{themeName === 'light' ? 'light_mode' : 'dark_mode'}</Icon>
