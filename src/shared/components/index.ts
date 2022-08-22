088d62aade105b72595ca2167b926c4aa7d3b56f
felipemalli <felipevahia@gmail.com>
2022-08-22 02:48:04 -0300

    feat: implement menu profile component

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -2,5 +2,6 @@ export * from './aside-drawer/AsideDrawer';
 export * from './detail-toolbar/DetailToolbar';
 export * from './header/Header';
 export * from './list-toolbar/ListToolbar';
+export * from './menu-profile/MenuProfile';
 export * from './search-layer/SearchLayer';
 
---
+++ src/shared/components/menu-profile/MenuProfile.tsx
@@ -0,0 +1,74 @@
+import { Logout } from '@mui/icons-material';
+import { Avatar, Divider, IconButton, ListItemIcon, Menu, MenuItem, Tooltip, useTheme } from '@mui/material';
+import { useState } from 'react';
+
+export const MenuProfile: React.FC = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const isOpen = Boolean(anchorEl);
+  const theme = useTheme();
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <Tooltip title="Account settings">
+        <IconButton onClick={handleClick}>
+          <Avatar 
+            sx={{ height: theme.spacing(5), width: theme.spacing(5) }}
+            src="/static/images/avatar/1.jpg" />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={isOpen}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem>
+          <Avatar /> Profile
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
